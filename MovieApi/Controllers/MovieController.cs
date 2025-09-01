2ba5cde9dec4d90ddf9d19941ac395d0e61d29d7
Chris <loppnow.cd@gmail.com>
2025-09-01 16:17:00 -0300

    Add data persistence to MovieController

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Api.Data;
 using MovieApi.Api.Services;
+using MovieApi.Api.Models;
 
 namespace MovieApi.Api.Controllers;
 
@@ -8,16 +10,37 @@ namespace MovieApi.Api.Controllers;
 public class MovieController : ControllerBase
 {
     private readonly MovieApiService _movieApiService;
+    private readonly MovieContext _context;
 
-    public MovieController(MovieApiService movieApiService)
+    public MovieController(MovieApiService movieApiService, MovieContext context) // adicionado 'MovieContext context' no construtor
     {
         _movieApiService = movieApiService;
+        _context = context; // adicionada esta linha pro context ser inicializado
     }
 
     [HttpGet("fetch-and-save-popular-movies")]
     public async Task<IActionResult> FetchAndSavePopularMovies()
     {
-        var response = await _movieApiService.GetPopularMoviesAsync();
-        return Ok(response);
+        var moviesFromApi = await _movieApiService.GetPopularMoviesAsync();
+
+        if (moviesFromApi == null || !moviesFromApi.Any())
+        {
+            return NotFound("No movies found from the API.");
+        }
+
+        foreach (var apiMovie in moviesFromApi)
+        {
+            var movie = new Movie
+            {
+                Title = apiMovie.Title,
+                Overview = apiMovie.Overview,
+                ReleaseDate = DateTime.TryParse(apiMovie.ReleaseDate, out var date) ? date : (DateTime?)null,
+                Popularity = apiMovie.Popularity
+            };
+            _context.Movies.Add(movie);
+        }
+
+        await _context.SaveChangesAsync();
+        return Ok("Movies have been successfully fetched and saved to the database.");
     }
 }
