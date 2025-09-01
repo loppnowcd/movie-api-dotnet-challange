05cf8556fac393310873a300fa9209d7d1dd704a
Chris <loppnow.cd@gmail.com>
2025-09-01 10:41:53 -0300

    Create Movie entity model

---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApi.Api.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Overview { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public double? Popularity { get; set; }
+}
