ede3ae8fd726c0b8fb14222329fbd4e677212d76
Chris <loppnow.cd@gmail.com>
2025-09-03 10:53:52 -0300

    Add data validation to Movie entity

--- MovieApi/Models/Movie.cs
+++ MovieApi/Models/Movie.cs
@@ -1,9 +1,13 @@
-﻿namespace MovieApi.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Api.Models;
 
 public class Movie
 {
     public int Id { get; set; }
+    [Required] // garante que o titulo nao seja nulo
     public string? Title { get; set; }
+    [StringLength(500, ErrorMessage = "A sinopse deve ter no máximo 500 caracteres.")]
     public string? Overview { get; set; }
     public DateTime? ReleaseDate { get; set; }
     public double? Popularity { get; set; }
